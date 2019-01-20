1927178686ad5daceee541653738061d14a9b150
Nikki Sharifli <nikkis@nulogy.com>
2019-01-20 14:36:24 -0500

    Change conditional statement

--- js/indexController.js
+++ js/indexController.js
@@ -1,9 +1,10 @@
 
-if(!navigator.serviceWorker) return;
-window.addEventListener('load', () => {
-  navigator.serviceWorker.register('/serviceWorker.js').then(() => {
-    console.log('ServiceWorker registration was successful!');
-  }).catch(err => {
-    console.log('ServiceWorker registration failed: ', err);
+if(navigator.serviceWorker) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/serviceWorker.js').then(() => {
+      console.log('ServiceWorker registration was successful!');
+    }).catch(err => {
+      console.log('ServiceWorker registration failed: ', err);
+    });
   });
-});
+}
