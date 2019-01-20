4efc0184ca704d226df1ca8e32ce09b249380300
Nikki Sharifli <nikkis@nulogy.com>
2019-01-20 14:34:23 -0500

    Add service worker

--- js/indexController.js
+++ js/indexController.js
@@ -1,7 +1,7 @@
 
 if(!navigator.serviceWorker) return;
 window.addEventListener('load', () => {
-  navigator.serviceWorker.register('/sw.js').then(() => {
+  navigator.serviceWorker.register('/serviceWorker.js').then(() => {
     console.log('ServiceWorker registration was successful!');
   }).catch(err => {
     console.log('ServiceWorker registration failed: ', err);
